0f65e8d6929dadcaa00f8c7e895d1c9dd9642bbf
UtpalTheDev <utpalpati8@gmail.com>
2021-01-08 23:53:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var input1=document.querySelector("#input1");
+var input2=document.querySelector("#input2");
+var button=document.querySelector("#button");
+var output=document.querySelector("#output");
+
+button.addEventListener("click",clickhandle);
+
+function clickhandle(){
+    var due=parseInt(input1.value);
+    var html="";
+    if(due>=1){
+        console.log(due);
+        var receive=parseInt(input2.value);
+        console.log(receive);
+        if(due >= receive)
+        {
+         //var receive=parseInt(read.question("receiving\n"));
+           // alert("You are giving lesser money than the due amount");
+            console.log("less");
+            
+        }
+        else
+        {
+            var ret=receive-due;
+            var arr=[1,5,10,20,100,500,2000];
+            //console.log(arr);
+            var max=0;
+            var new1=0;
+            var new2=0;
+        
+            function recr(ret1){
+            for(var i=0;i<arr.length;i++){
+                if(ret1>=arr[i]){
+                max=arr[i];
+                // console.log(max);
+                }
+            }
+            new1=parseInt(ret1/max);
+            new2=parseInt(ret1%max);
+            html+="<div>"+max+"*"+new1+"</div>";
+            console.log(max +"*"+new1);
+            if(new2>0){
+                recr(new2);
+            }
+            else{
+                output.innerHTML=html;
+                console.log("returning"+ret);
+                
+            }
+            
+            }
+            if(ret>=1){
+            recr(ret);
+            }
+            else{
+            console.log("no return");
+            }
+        } 
+    }
+    else{
+    console.log("no due no return")
+    }
+}   
